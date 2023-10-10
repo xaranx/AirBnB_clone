120751edc350b5866ffcac85f071beeddaab6b20
Hatimsel <selmun13@gmail.com>
2023-10-10 07:25:43 +0100

    fixing pull

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,8 +10,8 @@ class FileStorage:
     """
     Serializes and Deserializes JSON files
     """
-    __file_path = 'file.json'
-    objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def __init__(self):
         """
@@ -23,21 +23,20 @@ class FileStorage:
         """
         Returns the dictionary __objects
         """
-        return FileStorage.objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """
         Sets in __objects the obj with key obj class name.id
         """
-        name = obj.__class__.__name__
-        FileStorage.objects[name + '.' + str(obj.id)] = obj.__dict__
+        FileStorage.__objects[(obj.__class__.__name__).id] = obj
 
     def save(self):
         """
         Serializes __objects to the JSON file
         """
         with open(FileStorage.__file_path, 'w') as f:
-            json.dump(FileStorage.objects, f)
+            json.dump(FileStorage.__objects, f)
 
     def reload(self):
         """
@@ -45,6 +44,6 @@ class FileStorage:
         """
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r') as f:
-                FileStorage.objects = json.load(f)
+                FileStorage.__objects = json.load(f)
         else:
             pass
