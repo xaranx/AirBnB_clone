e4d3e49f493385dd7ab783cc2da7a78f22f87ed8
Hatimsel <selmun13@gmail.com>
2023-10-13 07:45:29 +0100

    Bug found and fixed thanks to Ryu

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,15 @@ class BaseModel:
     """
     Defines all common attributes/methods for other classes
     """
-    now = datetime.now()
-
     def __init__(self, *args, **kwargs):
         """
-        Instantiation of the class
+        The class constructor
+        Arguments: *args, **kwargs
         """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         if len(kwargs) > 0:
             for k, v in kwargs.items():
@@ -25,11 +28,9 @@ class BaseModel:
                 elif k == 'created_at' or k == 'updated_at':
                     setattr(self, k, datetime.strptime(v, time_format))
 
-                setattr(self, k, v)
-        
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+                else:
+                    setattr(self, k, v)
+
         from models.__init__ import storage
         storage.new(self)
 
