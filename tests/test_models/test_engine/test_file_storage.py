0810b4e54ca1248a8ab0b3e5b53b777b09a7a60f
Hatimsel <selmun13@gmail.com>
2023-10-15 16:01:59 +0100

    testing file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,7 +5,7 @@ Testing FileStorage class
 import models
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-from models.__init__ import storage
+# from models.__init__ import storage
 import os
 import unittest
 
@@ -18,11 +18,14 @@ class TestFileStorage(unittest.TestCase):
         """
         Testing basic tests on FileStorage class
         """
-        obj = BaseModel()
-        data = storage.all()
+        obj = FileStorage()
+        data = obj.all()
 
+        self.assertIsInstance(obj, FileStorage)
         self.assertTrue(type(data), dict)
         self.assertTrue(len(data) > 0)
+        self.assertTrue(hasattr(obj, f'_{obj.__class__.__name__}__objects'))
+        self.assertTrue(hasattr(obj, f'_{obj.__class__.__name__}__file_path'))
 
         obj.save()
         self.assertTrue(os.path.exists('file.json'))
